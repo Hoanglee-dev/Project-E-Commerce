1fd834941793fbd8750e40607819ea9c41cc6ab1
Hoangle-Dev <levuhoangks@gmail.com>
2025-05-05 14:35:37 +0700

    feat: build UI register-layout

--- src/layouts/RegisterLayout.tsx
+++ src/layouts/RegisterLayout.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Footer from '~/components/Footer'
+import RegisterHeader from '~/components/RegisterHeader'
 
 interface Props {
   children?: React.ReactNode
@@ -6,8 +8,9 @@ interface Props {
 export default function RegisterLayout({ children }: Props) {
   return (
     <div>
-      Register layout
+      <RegisterHeader />
       {children}
+      <Footer />
     </div>
   )
 }
