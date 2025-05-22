994dec706805f4ebb2c3bec69522d6cdab0c5f4b
Hoangle-Dev <levuhoangks@gmail.com>
2025-05-22 16:25:27 +0700

    feat: enhance ProductDetail and Product components with image navigation

--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -1,16 +1,18 @@
 import { useParams } from 'react-router-dom'
 
 import { formatCurrency, formatNumberToSocialStyle, rateSale } from '~/utils/utils'
-import Product from '../ProductList/components/Product/Product'
 import { useQuery } from '@tanstack/react-query'
 import productApi from '~/apis/product.api'
 import ProductRating from '../ProductList/components/ProductRating'
 import QuantityController from '~/components/QuantityController'
-import { ProductListConfig } from '~/types/product.type'
+import { Product as ProductType, ProductListConfig } from '~/types/product.type'
+import { useEffect, useMemo, useState } from 'react'
+import Product from '../ProductList/components/Product/Product'
 
 export default function ProductDetail() {
   const { id } = useParams()
-
+  const [currentIndexImages, setCurrentIndexImages] = useState([0, 5])
+  const [activeImage, setActiveImage] = useState('')
   const { data: productDetailData } = useQuery({
     queryKey: ['product', id],
     queryFn: () => productApi.getProductDetail(id as string)
@@ -25,6 +27,32 @@ export default function ProductDetail() {
     enabled: Boolean(product),
     staleTime: 2 * 60 * 1000
   })
+
+  const currentImages = useMemo(() => {
+    return product ? product.images.slice(...currentIndexImages) : []
+  }, [product, currentIndexImages])
+
+  useEffect(() => {
+    if (product && product.images.length > 0) {
+      setActiveImage(product.images[0])
+    }
+  }, [product])
+
+  const chooseActive = (img: string) => {
+    setActiveImage(img)
+  }
+
+  const handleNextImage = () => {
+    if (currentIndexImages[1] < (product as ProductType).images.length) {
+      setCurrentIndexImages((prev) => [prev[0] + 1, prev[1] + 1])
+    }
+  }
+  const handlePrevImage = () => {
+    if (currentIndexImages[0] > 0) {
+      setCurrentIndexImages((prev) => [prev[0] - 1, prev[1] - 1])
+    }
+  }
+
   if (!product) return null
   return (
     <div className='bg-gray-200 py-6'>
@@ -34,13 +62,16 @@ export default function ProductDetail() {
             <div className='col-span-5'>
               <div className='relative w-full cursor-zoom-in overflow-hidden  pt-[100%] shadow'>
                 <img
-                  src={product.image}
+                  src={activeImage}
                   alt={product.name}
                   className='absolute top-0 left-0 h-full w-full bg-white object-cover pointer-events-none'
                 />
               </div>
               <div className='relative mt-4 grid grid-cols-5 gap-1'>
-                <button className='absolute left-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'>
+                <button
+                  onClick={handlePrevImage}
+                  className='absolute left-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'
+                >
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
                     fill='none'
@@ -52,12 +83,13 @@ export default function ProductDetail() {
                     <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5L8.25 12l7.5-7.5' />
                   </svg>
                 </button>
-                {product.images.slice(0, 5).map((img, index) => {
-                  const isActive = index == 0
+                {currentImages.map((img, index) => {
+                  const isActive = img == activeImage
                   return (
-                    <div className='relative w-full pt-[100%]' key={index}>
+                    <div className='relative w-full pt-[100%]' onMouseEnter={() => chooseActive(img)} key={index}>
                       <img
                         src={img}
+                        alt={product.name}
                         className='absolute top-0 left-0 h-full w-full cursor-pointer bg-white object-cover'
                       />
                       {isActive && <div className='absolute inset-0 border-2 border-orange' />}
@@ -65,7 +97,10 @@ export default function ProductDetail() {
                   )
                 })}
 
-                <button className='absolute right-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'>
+                <button
+                  onClick={handleNextImage}
+                  className='absolute right-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'
+                >
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
                     fill='none'
