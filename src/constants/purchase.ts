8d488ed8037ccb2d0f95479a90a127fe4b24f2af
Hoangle-Dev <levuhoangks@gmail.com>
2025-05-23 15:52:49 +0700

    feat: add purchase API and types for managing purchase status and details

---
+++ src/constants/purchase.ts
@@ -0,0 +1,9 @@
+export const purchaseStatus = {
+  productInCart: -1,
+  productAll: 0,
+  productPendingConfirmation: 1,
+  productBeingPrepared: 2,
+  productInTransit: 3,
+  productDelivered: 4,
+  productCanceled: 5
+} as const
