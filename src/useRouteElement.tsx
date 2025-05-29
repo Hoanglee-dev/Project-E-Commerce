3617bffdccae918da17c9eefcd9c7531ae5b646a
Hoangle-Dev <levuhoangks@gmail.com>
2025-05-29 14:54:23 +0700

    feat: add user profile, change password, and history purchase pages with layout integration

--- src/useRouteElement.tsx
+++ src/useRouteElement.tsx
@@ -4,12 +4,15 @@ import ProductList from './pages/ProductList'
 import Register from './pages/Register'
 import RegisterLayout from './layouts/RegisterLayout'
 import MainLayout from './layouts/MainLayout'
-import Profile from './components/Profile'
 import { useContext } from 'react'
 import { AppContext } from './contexts/app.context'
 import path from './constants/path'
 import ProductDetail from './pages/ProductDetail'
 import Cart from './pages/Cart'
+import Profile from './pages/User/pages/Profile'
+import UserLayout from './pages/User/layouts'
+import HistoryPurchase from './pages/User/pages/HistoryPurchase'
+import ChangePassword from './pages/User/pages/ChangePassword'
 
 function ProtectedRoute() {
   const { isAuthenticated } = useContext(AppContext)
@@ -39,33 +42,43 @@ export default function useRouteElement() {
       )
     },
     {
-      path: path.profile,
-      element: <ProtectedRoute />,
-      children: [
-        {
-          path: path.profile,
-          element: (
-            <MainLayout>
-              <Profile />
-            </MainLayout>
-          )
-        }
-      ]
-    },
-    {
-      path: path.cart,
+      path: '',
       element: <ProtectedRoute />,
       children: [
         {
           path: path.cart,
+          children: [
+            {
+              element: (
+                <MainLayout>
+                  <Cart />
+                </MainLayout>
+              )
+            }
+          ]
+        },
+        {
+          path: path.user,
           element: (
             <MainLayout>
-              <Cart />
+              <UserLayout />
             </MainLayout>
-          )
+          ),
+          children: [
+            { path: path.changePassword, element: <ChangePassword /> },
+            {
+              path: path.profile,
+              element: <Profile />
+            },
+            {
+              path: path.historyPurchase,
+              element: <HistoryPurchase />
+            }
+          ]
         }
       ]
     },
+
     {
       path: '',
       element: <RejectedRoute />,
